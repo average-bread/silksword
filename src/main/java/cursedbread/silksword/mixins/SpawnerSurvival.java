5c9b17f6927385deb5c992633ed058d5a70253c8
Bigjango13 <bigjango13@gmail.com>
2024-07-10 13:48:00 -0700

    Update to 7.2pr2

--- src/main/java/cursedbread/silksword/mixins/SpawnerSurvival.java
+++ src/main/java/cursedbread/silksword/mixins/SpawnerSurvival.java
@@ -8,6 +8,7 @@ import net.minecraft.core.entity.player.EntityPlayer;
 import net.minecraft.core.item.ItemStack;
 import net.minecraft.core.player.gamemode.Gamemode;
 import net.minecraft.core.world.World;
+import net.minecraft.core.util.helper.Side;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,9 +20,8 @@ public abstract class SpawnerSurvival extends Block {
 		super(key, id, Material.stone);
 	}
 
-
-	@Inject(method = "blockActivated", at = @At(value = "HEAD", target = "blockActivated(Lnet/minecraft/core/world/World;Lnet/minecraft/core/entity/player/EntityPlayer;)V"), cancellable = true)
-	public void survivalActivated(World world, int x, int y, int z, EntityPlayer player, CallbackInfoReturnable<Boolean> cir) {
+	@Inject(method = "onBlockRightClicked", at = @At(value = "HEAD"), cancellable = true)
+	public void survivalActivated(World world, int x, int y, int z, EntityPlayer player, Side side, double xPlaced, double yPlaced, CallbackInfoReturnable<Boolean> cir) {
 		ItemStack check = new ItemStack(ItemsMod.mobSoul);
 		ItemStack stack = player.getHeldItem();
 		if (player.getHeldItem() != null && player.getHeldItem().isItemEqual(check)){
